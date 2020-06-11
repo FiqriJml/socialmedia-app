eb8f4816650c6cbff76c4c01f16257f2c1b67314
fiqrijml <jamalfiqri@gmail.com>
2020-06-11 15:30:10 +0800

    Membuat Fungsi Logout users

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -7,6 +7,9 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import { signOut } from '../../store/actions/authActions';
+import {connect} from 'react-redux';
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -19,7 +22,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function ButtonAppBar() {
+function Navigation(props) {
   const classes = useStyles();
 
   return (
@@ -32,9 +35,17 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Physikol
           </Typography>
-          <Button color="inherit">Logout</Button>
+          <Button color="inherit" onClick={props.signOut}>Logout</Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Navigation);
